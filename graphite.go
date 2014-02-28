cc6102686f5928d09286ac816b76d2ee6e2b5f4a
Christian Wellenbrock <christian.wellenbrock@gmail.com>
2014-02-28 16:36:51 +0100

    Add Metric.String() function for quick logging

--- graphite.go
+++ graphite.go
@@ -25,6 +25,15 @@ type Metric struct {
 	Timestamp int64
 }
 
+func (metric Metric) String() string {
+	return fmt.Sprintf(
+		"%s %s %s",
+		metric.Name,
+		metric.Value,
+		time.Unix(metric.Timestamp, 0).Format("2006-01-02 15:04:05"),
+	)
+}
+
 // defaultTimeout is the default number of seconds that we're willing to wait
 // before forcing the connection establishment to fail
 const defaultTimeout = 5
